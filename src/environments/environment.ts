f9d5c8efbd4b97ea39fe16708c11fbcdd7c54dd7
JehielL <jehiel.py@gmail.com>
2026-01-18 14:32:55 +0100

    Refactor url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   googleClientId: '175213521481-9kqr7ggavg2elvar8h6ubp8aul8nsvcs.apps.googleusercontent.com',
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://api.fudi.es'
 };
